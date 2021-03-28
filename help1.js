948cdfda839167b53da914dc4570d8f3cb5025f4
HOSHIGAKILINDONA <81394121+HOSHIGAKILINDONA@users.noreply.github.com>
2021-03-28 01:28:20 -0200

    Add files via upload

---
+++ help1.js
@@ -0,0 +1,44 @@
+const help1 = (prefix) => {
+
+	return `
+╭────────────────╮
+ *COMANDOS HOSHIGAKI LINDONA*
+╰────────────────╯
+ 
+☂️ *${prefix}marcar*
+☂️ *${prefix}marcar2*
+☂️ *${prefix}marcar3*
+☂️ *${prefix}loli*
+☂️ *${prefix}loli1*
+☂️ *${prefix}hentai*
+☂️ *${prefix}dono*
+☂️ *${prefix}porno*
+☂️ *${prefix}boanoite*
+☂️ *${prefix}bomdia*
+☂️ *${prefix}boatarde*
+☂️ *${prefix}mia*
+☂️ *${prefix}mia1*
+☂️ *${prefix}mia2*
+☂️ *${prefix}belle*
+☂️ *${prefix}belle1*
+☂️ *${prefix}belle2*
+☂️ *${prefix}belle3*
+☂️ *${prefix}akeno*
+☂️ *${prefix}meme*
+☂️ *${prefix}lofi*
+☂️ *${prefix}malkova*
+☂️ *${prefix}canal*
+☂️ *${prefix}nsfwloli*
+☂️ *${prefix}reislin*
+☂️ *${prefix}limpar*
+☂️ *${prefix}marcar*
+☂️ *${prefix}ts (texto que deseja transmitir)*
+
+════════════════════
+*HOSHIGAKI BAIANA* 😳
+*Digite ${prefix}dono para mais info*
+════════════════════`
+
+}
+exports.help1 = help1
+
